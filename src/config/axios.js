47d66e951fd2e80b6fe0f357e5d32923f7d6176f
Halimat Ige <halimatyetundeige@gmal.com>
2025-10-05 18:09:55 +0100

    fix:removed a comma

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,11 +1,46 @@
+// config/axios.js
 import axios from 'axios';
 import { API_URL } from './constants';
 
-axios.defaults.baseURL = API_URL;
-axios.defaults.withCredentials = true;
-axios.defaults.headers.common['Content-Type'] = 'application/json';
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
 
-export default axios;
+// Add response interceptor for token refresh
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      
+      try {
+        await axiosInstance.get('/api/auth/refresh');
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
+
+// import axios from 'axios';
+// import { API_URL } from './constants';
+
+// axios.defaults.baseURL = API_URL;
+// axios.defaults.withCredentials = true;
+// axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+// export default axios;
 
 // import axios from 'axios';
 // import { API_URL } from './constants';
